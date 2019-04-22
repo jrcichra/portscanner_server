0d289a327e1dc188eb52e1d3a589a8865c84c53e
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2019-04-22 09:04:48 -0400

    Made for interactive runs

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 import socket
 import threading
 import json
-import itertools as it
+import re
+import signal
+import sys
 
+reg = r'^([\w]+.[\w]+)'
 server_port = 5555
-server_ip = '144.217.81.98'
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
 
 
 def scan_port(client_address, port):
@@ -22,25 +29,20 @@ def check_client(client_socket, client_address):
     o = {}
     o['ip'] = client_address
     o['open'] = []
-    if client_address == server_ip:  # this is to prevent a chain reaction
-        for port in it.chain(range(1, server_port), range(server_port+1, 65536)):
-            res = scan_port(client_address, port)
-            if(res):
-                o['open'].append(port)
-    else:
-        for port in range(1, 65536):
-            res = scan_port(client_address, port)
-            if(res):
-                o['open'].append(port)
+    for port in range(1, 65536):
+        status = '\rscanning port: ' + str(port)
+        client_socket.sendall(status.encode())
+        res = scan_port(client_address, port)
+        if(res):
+            found = '\n open: ' + str(port) + '\n'
+            client_socket.sendall(found.encode())
 
-    j = json.dumps(o, indent=4)
-    print(j)
-    client_socket.sendall(j.encode())
     client_socket.close()
 
 
 if __name__ == '__main__':
-
+    # control c listener
+    signal.signal(signal.SIGINT, signal_handler)
     # setup the socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -53,6 +55,11 @@ if __name__ == '__main__':
 
         print("Got a connection from " + str(client_address[0]))
 
-        t = threading.Thread(target=check_client, args=(
-            client_socket, client_address[0]))
-        t.start()
+        request = client_socket.recv(1024)
+        print(request)
+        if('portscan' in request.decode()):
+            t = threading.Thread(target=check_client, args=(
+                client_socket, client_address[0]))
+            t.start()
+        else:
+            client_socket.close()
